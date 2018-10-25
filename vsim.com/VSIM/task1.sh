7b80dc16c786fddeaaf9c4bb3a7455a9356cf368
azzatha <31382680+azzatha@users.noreply.github.com>
2018-10-25 15:13:51 +0300

    Update task1.sh

--- vsim.com/VSIM/task1.sh
+++ vsim.com/VSIM/task1.sh
@@ -13,7 +13,7 @@ echo "start $1"
 #groovy ./1-ClinVar/loadModeInh.groovy | grep "^OMIM" > ./1-ClinVar/omim_mode.txt
 
 # Find the intersect with CV Database,and Run Python of the input file for CV
-/var/www/vsim.com/bedtools2/bin/bedtools intersect -wb -f 1.00 -r -a ./AllDataBases/CV_DB.vcf -b $1 > ./1-ClinVar/clinvar_Data.vcf
+bedtools2/bin/bedtools intersect -wb -f 1.00 -r -a ./AllDataBases/CV_DB.vcf -b $1 > ./1-ClinVar/clinvar_Data.vcf
 cut -f9,10,11,12,13,14,15,16,17,18 ./1-ClinVar/clinvar_Data.vcf > ./1-ClinVar/clinvar_Data2.vcf
 egrep -v "^#" ./AllDataBases/CV_DB.vcf > ./AllDataBases/CV_Pathogenic_.vcf
 cat ./AllDataBases/headerCV.txt ./AllDataBases/CV_Pathogenic_.vcf >  ./AllDataBases/CV_DB_Anno.vcf
@@ -22,7 +22,7 @@ cat ./1-ClinVar/headerInfo.txt ./1-ClinVar/clinvar_Data2.vcf > ./1-ClinVar/clinv
 #Annotate the resulting file:
 bgzip ./AllDataBases/CV_DB_Anno.vcf
 tabix -p vcf ./AllDataBases/CV_DB_Anno.vcf.gz
-/usr/bin/vcfanno_linux64 -permissive-overlap ./AllDataBases/conf_ClinVar.toml  ./1-ClinVar/clinvar_Data_Annotate.vcf > ./1-ClinVar/Annotat_$1
+vcfanno_linux64 -permissive-overlap ./AllDataBases/conf_ClinVar.toml  ./1-ClinVar/clinvar_Data_Annotate.vcf > ./1-ClinVar/Annotat_$1
 egrep -v "^#" ./1-ClinVar/Annotat_$1 > ./1-ClinVar/Annotat_$1_noHeader.vcf
 python ./1-ClinVar/CV_NewData.py $1
 
@@ -36,17 +36,17 @@ rm ./AllDataBases/CV_DB_Anno.vcf.gz.tbi
 
 # 2- GWAS Work
 # Find the intersect with GWAS  Database, and Run Python of the input file for GW
-/var/www/vsim.com/bedtools2/bin/bedtools intersect -wb -wa -f 1.0 -r -a ./AllDataBases/GWAS_DB.vcf  -b $1 > ./2-Gwas/gwas_Data_$1
+bedtools intersect -wb -wa -f 1.0 -r -a ./AllDataBases/GWAS_DB.vcf  -b $1 > ./2-Gwas/gwas_Data_$1
 python ./2-Gwas/GW_NewData.py $1 
 
 # 3- PharmGKP Work
 # Find the intersect with PharmGKP  Database, and Run Python of the input file for PG
-/var/www/vsim.com/bedtools2/bin/bedtools intersect -wb -wa -f 1.0 -r -a ./AllDataBases/Pharm_DB.vcf -b  $1 > ./3-PharmGKP/PhG_$1
+bedtools intersect -wb -wa -f 1.0 -r -a ./AllDataBases/Pharm_DB.vcf -b  $1 > ./3-PharmGKP/PhG_$1
 python ./3-PharmGKP/PharmWork.py $1
 
 # 4- Dida Work
 # Find the intersect with Dida  Database, and Run Python of the input file for Di
-/var/www/vsim.com/bedtools2/bin/bedtools intersect -wa -wb -f 1.00 -r -a ./AllDataBases/DIDA_DB.vcf -b  $1 > ./4-Dida/Di_$1
+bedtools intersect -wa -wb -f 1.00 -r -a ./AllDataBases/DIDA_DB.vcf -b  $1 > ./4-Dida/Di_$1
 python ./4-Dida/DI_NewData.py $1
 
 # 5- MCAP annotation
